8e48653355cb2cb43904c1615d4d7769c1bda9de
LauraPostigo <166739141+LauraPostigo@users.noreply.github.com>
2025-01-14 21:50:04 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
@@ -11,10 +11,102 @@ const Home = () => {
 	const [inputValue, setInputValue]= useState("") 
 	const [ToDo, setToDo]= useState([]) 
 	
+// crear usuario
+function user() {
+  const requestOptions = {
+    method: "POST",
+    redirect: "follow"
+  };
+  
+  fetch("https://playground.4geeks.com/todo/users/LauraPostigo", requestOptions)
+    .then((response) => response.json())
+    .then((data) => ToDo(data.ToDo))
+    .catch((error) => console.error(error));
+} 
+
+// traer listado de tareas (get)
+function getListTodoList () {
+  const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify({
+  "name": "",
+  "todos": [
+    {
+      "label": "",
+      "is_done": false,
+      "id": 0
+    }
+  ]
+});
+
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("https://playground.4geeks.com/todo/users/LauraPostigo", requestOptions)
+  .then((response) => response.json())
+  .then((ToDo) => data(data.ToDo))
+  .catch((error) => console.error(error));
+}
+
+// fin listado tareas
+
+
+  // crear tarea
+function postListTodoList() {
+  const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify({
+  "label": "",
+  "is_done": false
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("https://playground.4geeks.com/todo/todos/LauraPostigo", requestOptions)
+  .then((response) => response.json())
+  .then((ToDo) => data(data.ToDo))
+  .catch((error) => console.error(error));
+}
+
+useEffect(()=>{
+  getListTodoList()
+
+},[])
+// fin crear tarea
+
+// borrar tarea (delete)
+
+function erasetask () {
+  const raw = "";
+
+const requestOptions = {
+  method: "DELETE",
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("https://playground.4geeks.com/todo/todos/43", requestOptions)
+  .then((response) => response.json())
+  .then((ToDo) => data(data.ToDo))
+  .catch((error) => console.error(error));
+}
+
+// fin borrar tarea
 
 
 
-	return (
+return (
 		<div className="container my-5">
   <h1 className="text-center text-muted">Quereseres</h1>
   <div className="bg-white shadow-sm p-4 rounded">
